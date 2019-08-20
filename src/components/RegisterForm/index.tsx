8f1811a825407ca66248a0e9d2b1272dafc19306
Douglas Lopes <dwillian.lopes@gmail.com>
2019-08-19 22:00:02 -0300

    feat: just using text input from Material UI

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -43,25 +43,25 @@ const RegisterForm = ({ user }: EditUserDialogProps) => {
           console.log('Subminting');
         }}
         render={({ errors, status, touched, isSubmitting }) => (
-          <Form>
-            <Field name='firstName' type='text' label='First Name' component={CustomInputComponent} />
-            <ErrorMessage name="firstname" component="div" />  
-            <Field type="text" name="lastname" label='Last Name' component={CustomInputComponent}/>
-            <ErrorMessage name="lastname" component="div"/> 
-            <Field type="string" name="telephone" label='Telephone' component={CustomInputComponent}/>
-            <ErrorMessage name="telephone" component="div"/>
-            <Field type="string" name="address" label='Address' component={CustomInputComponent}/>
-            <ErrorMessage name="address" component="div"/>
-            <Field type="number" name="number" label='Number' component={CustomInputComponent}/>
-            <ErrorMessage name="number," component="div"/>
-            <Field type="string" name="zipcode" label='Zip-Code' component={CustomInputComponent}/>
-            <ErrorMessage name="zipcode," component="div"/>
-            <Field type="string" name="city" label='City' component={CustomInputComponent}/>
-            <ErrorMessage name="city" component="div"/>  
-            {status && status.msg && <div>{status.msg}</div>}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            <Form>
+                <Field name='firstName' type='text' label='First Name' component={CustomInputComponent} />
+                <ErrorMessage name="firstname" component="div" />  
+                <Field type="text" name="lastname" label='Last Name' component={CustomInputComponent}/>
+                <ErrorMessage name="lastname" component="div"/> 
+                <Field type="string" name="telephone" label='Telephone' component={CustomInputComponent}/>
+                <ErrorMessage name="telephone" component="div"/>
+                <Field type="string" name="address" label='Address' component={CustomInputComponent}/>
+                <ErrorMessage name="address" component="div"/>
+                <Field type="number" name="number" label='Number' component={CustomInputComponent}/>
+                <ErrorMessage name="number," component="div"/>
+                <Field type="string" name="zipcode" label='Zip-Code' component={CustomInputComponent}/>
+                <ErrorMessage name="zipcode," component="div"/>
+                <Field type="string" name="city" label='City' component={CustomInputComponent}/>
+                <ErrorMessage name="city" component="div"/>  
+                {status && status.msg && <div>{status.msg}</div>}
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
           </Form>
         )}
       />
